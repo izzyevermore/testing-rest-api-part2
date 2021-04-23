8a9e83aa6be5a6b9a89fe8573a613703ab561b12
Isaiah <isaiahlekay2001@gmail.com>
2021-04-23 10:35:57 +0200

    different code with different tests

---
+++ starter_code/security.py
@@ -0,0 +1,24 @@
+from werkzeug.security import safe_str_cmp
+from starter_code.models.user import UserModel
+
+def authenticate(username, password):
+    '''
+    Function that gets called when a user calls the /auth endpoint
+    with their username and password
+    :param username: User's username in string format
+    :param password: User's unencrypted password in string format
+    :return: A user if authentication was successful, Non otherwise
+    '''
+    user = UserModel.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+def identity(payload):
+    '''
+    Function that gets called when a user has already authenticated,
+    and Flask-JWT verified their authorization header is correct.
+    :param payload: A dictionary with 'identity' key, which is the user id.
+    :return: A UserModel object.
+    '''
+    user_id = payload('identity')
+    return UserModel.find_by_id(user_id)
